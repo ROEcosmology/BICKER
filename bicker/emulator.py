65a79d0d0e64b7919dcc528a4ee94af3d2474665
Jamie <jamie.donald-mccann@port.ac.uk>
2022-02-17 16:48:11 +0000

    Added bicker v0.0.1

---
+++ bicker/emulator.py
@@ -0,0 +1,82 @@
+'''
+Flie contains Classes for the idividual component emulators.
+'''
+from tensorflow.keras.models import load_model
+import numpy as np
+from .training_funcs import UniformScaler, LogScaler
+from scipy.interpolate import interp1d
+import os
+import pathlib
+
+# Path to directory containing the NN weights as well as scalers needed produce
+#  predictions with the NNs.
+cache_path = os.fsdecode(pathlib.Path(os.path.dirname(__file__)
+                                      ).parent.absolute())+"/bicker-cache/"
+
+kbins = np.hstack([np.logspace(np.log(3.0e-4), np.log(.005), 4, base=np.e),
+                   np.arange(.00501,.301,.0025),
+                   np.logspace(np.log(.301), np.log(10.0), 25, base=np.e)])
+
+class component_emulator:
+    '''
+    Class componenet emulator.
+
+    On initalisation the weights for the NN will be loaded,
+    along with the scalers required to make predictions with the NN.
+
+    Args:
+        kernel (str) : String to specify what component.
+    '''
+
+    def __init__(self, kernel):
+
+        self.kbins = kbins
+        '''The k-bins at which predictions will be made.'''
+
+        components_path = cache_path+"components/"
+
+        # Load the NN.
+        model = load_model(components_path+kernel+"/member_0", compile=False)
+
+        self.model = model
+
+        scalers_path = cache_path+"scalers/"
+
+        xscaler = UniformScaler()
+        yscaler = UniformScaler()
+
+        # Load the variables that define the scalers.
+        xmin_diff = np.load(scalers_path+"xscaler_min_diff.npy")
+        ymin_diff = np.load(scalers_path+kernel+"/yscaler_min_diff.npy")
+
+        xscaler.min_val = xmin_diff[0, :]
+        xscaler.diff = xmin_diff[1, :]
+
+        yscaler.min_val = ymin_diff[0, :]
+        yscaler.diff = ymin_diff[1, :]
+
+        self.scalers = (xscaler, yscaler)
+
+    def emu_predict(self, X):
+        '''
+        Make predictions with the component emulator.
+
+        Args:
+            X (array) : Array containing the relevant input parameters. If making
+             a single prediction should have shape (d,), if a batch prediction
+             should have the shape (N,d).
+
+        Returns:
+            Array containing the predictions from the component emulator.
+        '''
+
+        # If making a prediction on single parameter set, input array needs to
+        # be reshaped.
+        X = np.atleast_2d(X)
+
+        X_prime = self.scalers[0].transform(X)
+
+        preds = self.scalers[1].inverse_transform(
+            self.model(X_prime))
+
+        return preds
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", 'r') as dependencies:
+    requirements = [pkg.strip() for pkg in dependencies]
+
+setuptools.setup(
+    name="bicker",
+    version="0.0.1",
+    author="Jamie Donald-McCann",
+    author_email="jamie.donald-mccann@port.ac.uk",
+    description="BIspeCtrum KERnel emulator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ROEcosmology/BICKER",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+    install_requires=requirements,
+    python_requires='>=3.7',
+)
